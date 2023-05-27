95d276d3db46657adb4651b3b983c696f537d6f9
iqpizza6349 <iqpizza6349@gmail.com>
2023-05-27 21:03:10 +0900

    :necktie: :: update dynamic finder function by reading js files

---
+++ src/modules/reflections/inheritorReflector.js
@@ -0,0 +1,24 @@
+import * as path from 'path';
+import * as fs from 'fs';
+import 'reflect-metadata';
+
+export function discoverUnusedClasses(directory) {
+    const classes = [];
+
+    const files = fs.readdirSync(directory);
+
+    for (const file of files) {
+        if (path.extname(file) === '.js') {
+            const dynamic = require(`../commands/${file}`);
+            const module = Object.values(dynamic)[0];
+            if (module && file !== 'commandLoader.js') {
+                const instance = new module();
+                if (instance && instance.cmd) {
+                    classes.push(instance);
+                }
+            }
+        }
+    }
+
+    return classes;
+}
