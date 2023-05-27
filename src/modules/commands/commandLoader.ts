bfa2bd73e41a705098a2707bbb223f03ec5d4244
iqpizza6349 <iqpizza6349@gmail.com>
2023-05-27 21:09:11 +0900

    :necktie: :: add command loader file to dynamic command loader

---
+++ src/modules/commands/commandLoader.ts
@@ -0,0 +1,13 @@
+import { discoverUnusedClasses } from "../reflections/inheritorReflector";
+import { ICommand } from './ICommand';
+
+export function findCommands(): any[] {
+    return findDynamicCommands().map((v) => {
+        return {name: v.name(), description: v.description()};
+        // return new Command(v.name(), v.description());
+    });
+}
+
+function findDynamicCommands(): ICommand[] {
+    return discoverUnusedClasses(__dirname);
+}
