9a25d5b503c8c604adb447dfbe28e48d37b465b8
iqpizza6349 <iqpizza6349@gmail.com>
2023-05-27 21:13:55 +0900

    :sparkles: :: add dynamic commands loading, deploy in bot. and also deploy interaction points

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,23 @@
 import { Client, GatewayIntentBits } from "discord.js";
 import { config } from 'dotenv';
 import { InteractionRegister } from "./modules/interactions/interactionRegister";
-import { ICommand } from "./modules/commands/ICommand";
-import { Ping } from "./modules/commands/ping";
-import { getCommands, getDetails } from "./modules/commands/decorator/cmd";
+import { CommandRegister } from "./modules/commands/commandRegister";
+import { findCommands } from "./modules/commands/commandLoader";
 
 config();   // config '.env'
+
+const TOKEN = process.env.BOT_TOKEN || '';
+const CLIENT_ID = process.env.CLIENT_ID || '';
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 const interactionManager = new InteractionRegister(client);
 
+// dynamic find commands and register it
+const dynamicCommands = findCommands();
+const commandRegister = new CommandRegister(TOKEN, CLIENT_ID, dynamicCommands);
+commandRegister.registerCommands();
+
 // add interactions to work in bot
 interactionManager.addReadyInteraction();
 
-const ping = new Ping();
-console.log(ping.name());
-console.log(getCommands());
-console.log(getDetails());
-
-client.login(process.env.BOT_TOKEN);
+client.login(TOKEN);
