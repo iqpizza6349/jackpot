f20c8ffd21ccce86b20c5ae213fa381f9ee5e2dc
iqpizza6349 <iqpizza6349@gmail.com>
2023-05-28 23:08:21 +0900

    :construction: :: game play function work in progress

---
+++ src/modules/commands/game.ts
@@ -0,0 +1,53 @@
+import { CommandInteraction, CacheType, ApplicationCommandOptionType } from "discord.js";
+import { ICommand } from "./ICommand";
+import { Game } from "../database/schemas/Game";
+
+export class PlayGame implements ICommand {
+    cmd: boolean = true;
+    name(): string {
+        return "토토개장";
+    }
+
+    description(): string {
+        return "토토 판을 개장합니다. 관리자 이외에는 사용할 수 없습니다.";
+    }
+
+    options(): any[] {
+        return [
+            {
+                name: "토토명",
+                description: "개장할 토토의 이름을 작성하세요",
+                type: ApplicationCommandOptionType.String,
+                required: true
+            }
+        ];
+    }
+
+    async action(interation: CommandInteraction<CacheType>): Promise<void> {
+        if (interation.commandName === this.name()) {
+            // TODO: check is interaction user is manager
+            const gameName = interation.options.get("토토명")?.value as string;
+            const isExists = await this.existsGame(gameName);
+            if (!isExists) {
+                await interation.reply({ content: "해당 토토명으로 이미 개장되었습니다.", ephemeral: true });
+                return;
+            }
+
+            await Game.create({
+                name: gameName
+            });
+            await interation.channel?.send(`@everyone \`${gameName}\` 토토판이 개장되었습니다!`);
+            await interation.reply({ content: this.messageFormat(gameName), ephemeral: true });
+        }
+    }
+
+    async existsGame(gameName: string): Promise<boolean> {
+        const game = await Game.find({ name: gameName });
+        return (game.length === 0);
+    }
+
+
+    private messageFormat(gameName: string): string {
+        return `토토 \`${gameName}\`판이 개장되었습니다!`;
+    }
+}
