27dd5b1b15ed9038ef08ea117e552b77591583df
iqpizza6349 <iqpizza6349@gmail.com>
2023-06-02 14:36:46 +0900

    :sparkles: :: add ranking slash-command

--- src/modules/commands/rank.ts
+++ src/modules/commands/rank.ts
@@ -1,27 +1,64 @@
-import { CommandInteraction, CacheType } from "discord.js";
+import { CommandInteraction, CacheType, EmbedBuilder, Client } from "discord.js";
 import { ICommand } from "./ICommand";
+import { Player } from "../database/schemas/Player";
 
+/**
+ * 전체 플레이어의 소지금을 내림차순으로 정렬하여, 순위를 10위까지
+ * 계산하여 출력.
+ * 본인의 순위는 임베드(embed)기준, footer 위치에 출력
+ */
 export class Rank implements ICommand {
     cmd: boolean = true;
 
     name(): string {
-        throw new Error("Method not implemented.");
+        return "랭킹";
     }
 
     description(): string {
-        throw new Error("Method not implemented.");
+        return "전체 플레이어의 소지금 순위 및 본인 순위";
     }
 
     options(): any[] {
-        throw new Error("Method not implemented.");
+        return [];
     }
 
     permission(): bigint | undefined {
-        throw new Error("Method not implemented.");
+        return undefined;
     }
 
-    action(interation: CommandInteraction<CacheType>): Promise<void> {
-        throw new Error("Method not implemented.");
+    async action(interation: CommandInteraction<CacheType>): Promise<void> {
+        if (interation.commandName !== this.name()) {
+            return;
+        }
+        
+        const rankers = await this.findTop10Players();
+        const self = await this.selfRank(Number(interation.user.id));
+
+        const embed = new EmbedBuilder().setColor(0x0099FF)
+                        .setTitle("토토 랭킹")
+                        .setDescription("모든 플레이어별 소지금 랭킹")
+                        .addFields(this.addRankers(interation.client, rankers))
+                        .setFooter({
+                            text: `${interation.user.username}님은 ${self + 1}위입니다.`
+                        });
+        interation.reply({ embeds: [embed] });
+    }
+
+    private async findTop10Players(): Promise<any[]> {
+        return await Player.find({}).sort({ amount: -1 }).limit(10);
+    }
+
+    private async selfRank(selfScore: number) {
+        return await Player.count({ amount: { $gt: selfScore }})
+                            .sort({ amount: -1 });
+    }
+
+    private addRankers(client: Client<true>, rankers: any[]) {
+        return rankers.map((v, i) => {
+            return { 
+                name: `${i + 1}위`,
+                value: `${client.users.cache.get(v._id)?.username}`
+            };
+        });
     }
 }
-
