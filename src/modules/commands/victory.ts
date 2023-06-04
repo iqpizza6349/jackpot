0573ef7066e0f365fbdac544eb3c23b557f6c16f
iqpizza6349 <iqpizza6349@gmail.com>
2023-06-04 14:37:29 +0900

    :necktie: :: add interation replies before returns

--- src/modules/commands/victory.ts
+++ src/modules/commands/victory.ts
@@ -1,4 +1,4 @@
-import { CommandInteraction, CacheType, ApplicationCommandOptionType, PermissionFlagsBits } from "discord.js";
+import { CommandInteraction, CacheType, ApplicationCommandOptionType, PermissionFlagsBits, EmbedBuilder } from "discord.js";
 import { ICommand } from "./ICommand";
 import { Game } from "../database/schemas/Game";
 import { GameTeam } from "../database/schemas/GameTeam";
@@ -39,10 +39,12 @@ export class Victory implements ICommand {
         const currentGame = await this.findOpenedGame();
         if (currentGame === null) {
             // 현재 진행 중인 게임을 찾지 못함
+            interation.reply({ content: "현재 진행 중인 게임을 찾지 못하였습니다.", ephemeral: true });
             return;
         }
         else if (currentGame.open === true) {
             // 배팅 중인 게임
+            interation.reply({ content: "현재 배팅 중인 게임입니다. /배팅중지 로 먼저 배팅을 중지해주세요!", ephemeral: true });
             return;
         }
 
@@ -51,14 +53,31 @@ export class Victory implements ICommand {
         const isExists: boolean = allGameTeam.filter((g) => g.name === winningTeam).length !== 0;
         if (!isExists) {
             // '승리한팀'이 현재 게임에 존재하지 않음
+            interation.reply({ content: `현재 게임에는 ${winningTeam}(팀)이 존재하지 않습니다.`, ephemeral: true });
             return;
         }
 
-        await this.findAllPlayers(currentGame._id, winningTeam);
-
+        const bettingAmount = await this.findAllPlayers(currentGame._id, winningTeam);
         await currentGame.updateOne({ finish: true });
+        const bettings = bettingAmount as Map<string, number>;
         // send 'current game has finished... which team has win..?' embed message
-        interation.reply({ content: 'nothing', ephemeral: true });
+        const allAmount = Array.from(bettings.values()).reduce((prev, cur) => {
+            return Number(prev) + Number(cur);
+        }, 0);
+        const embed = this.createEmbed(winningTeam, allAmount);
+        interation.reply({ embeds: [embed] });
+    }
+
+    private createEmbed(winningTeam: string, bettingAmount: number) {
+        return new EmbedBuilder()
+                .setColor(0x0099FF)
+                .setTitle("토토 종료")
+                .setDescription("어느 팀이 승리했는 지 정보를 출력합니다.")
+                .addFields(
+                    { name: "우승팀", value: `${winningTeam}(팀)`, inline: true },
+                    { name: "총 배팅액", value: `${bettingAmount}` },
+                    { name: "\u200B", value: "\uD83C\uDF89 승리팀에 배팅한 분들 모두 축하드립니다." }
+                );
     }
 
     private async findOpenedGame() {
@@ -87,7 +106,7 @@ export class Victory implements ICommand {
                                         bettingAmount);
                             for (let player of players) {
                                 const gameRecord = this.findGameRecords(gameId, player.history)[0];
-                                if(gameRecord.index === -1) {
+                                if(gameRecord === undefined || gameRecord === null || gameRecord.index === -1) {
                                     continue;
                                 }
 
@@ -120,6 +139,7 @@ export class Victory implements ICommand {
                                     { $set: { won: true } }
                                 );
                             }
+                            return bettingAmount;
                         });
     }
 
@@ -127,7 +147,7 @@ export class Victory implements ICommand {
         for (let player of players) {
             const history = player.history;
             const gameRecords = this.findGameRecords(gameId, history);
-            if (gameRecords[0].index === -1) {
+            if (gameRecords.length === 0 || gameRecords[0].index === -1) {
                 // it means empty
                 continue;
             }
