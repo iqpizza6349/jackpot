dc46e64c15ca2b95d2bce97d9e59c3499b7d0f11
iqpizza6349 <iqpizza6349@gmail.com>
2023-06-04 14:40:43 +0900

    :ambulance: :: hotfix when modfies betting amount or whatever

--- src/modules/commands/bet.ts
+++ src/modules/commands/bet.ts
@@ -82,7 +82,7 @@ export class Bet implements ICommand {
             const previousTeam = record.team.name;
             const previousAmount = record.amount;
             await record.updateOne({ team: team._id, amount: bettingAmount });
-            await player.updateOne({ amount: player.amount - bettingAmount });
+            await player.updateOne({ amount: record.amount + player.amount - bettingAmount });
             await interation.reply(
                 {
                     content: `배팅을 ${previousTeam}, ${previousAmount}원 -> ${team.name}, ${bettingAmount}원으로 변경하셨습니다.`,
